64e4d8a45f7ba32ab71a719a84faef0dcc69d2c5
Dknwhe <cai.mac@hotmail.com>
2020-02-23 17:59:17 +0100

    Some Change

--- src/main/java/se/ecutb/cai/data/DataBase.java
+++ src/main/java/se/ecutb/cai/data/DataBase.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class DataBase {
 
     private static String USERNAME = "root";
-    private static String PASSWORD = "pellepelle12";
+    private static String PASSWORD = "1234";
     private static final String URL = "jdbc:mysql://localhost:3306/world?&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Europe/Berlin";
 
 
